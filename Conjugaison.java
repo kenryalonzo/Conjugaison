f932e2256b42f2d67139938cf9a2be7c1b097437
GHOST <kenryalonzo@gmail.com>
2023-11-02 00:18:06 +0100

    fix: I've updated my code to check whether the verb entered is a first-group verb before conjugating.

--- Conjugaison.java
+++ Conjugaison.java
@@ -5,25 +5,32 @@ public class Conjugaison {
     public static void main (String[] args) {
         
         Scanner readScanner = new Scanner(System.in);
-        System.out.println("veuiller entrer un verbe du premier groupe");
+        System.out.println("VeuilleZ entrer un verbe du premier groupe");
         String verbe = readScanner.nextLine();
 
-        // essayons de creer un radical pour le verbe qu'on recupere
+        // now we need to test if the verber finish by "er"
+        if(!verbe.endsWith("er")) {
 
-        String radical = verbe.substring(0, verbe.length() -2);
-
-        // maintenant on cree deux tableaux a parcourir pour la conjugaison du verbe
-
-        String[] prounoums = new String[]{"Je", "Tu", "Il/Elle", "Nous", "Vous", "Ils/Elles"};
-        String[] terminaisons = new String[]{"e", "es", "e", "ons", "ez", "ent"};
-        
-        // Maintenaint nous creer une brique de code permettant l'affichage de la conjugaison du verbe
-
-        for (int i = 0; i < 6; i++) {
-            System.out.println(prounoums[i] + " " + radical + terminaisons[i]);
+            System.out.println("Le verbe doit obligatoirement se terminer par |er|");
         }
+        else {
 
-        
+            // essayons de creer un radical pour le verbe qu'on recupere
+
+            String radical = verbe.substring(0, verbe.length() -2);
+
+            // maintenant on cree deux tableaux a parcourir pour la conjugaison du verbe
+
+            String[] prounoums = new String[]{"Je", "Tu", "Il/Elle", "Nous", "Vous", "Ils/Elles"};
+            String[] terminaisons = new String[]{"e", "es", "e", "ons", "ez", "ent"};
+            
+            // Maintenaint nous creer une brique de code permettant l'affichage de la conjugaison du verbe
+
+            for (int i = 0; i < 6; i++) {
+                System.out.println(prounoums[i] + " " + radical + terminaisons[i]);
+            }
+        }
+  
     }
     
 }
